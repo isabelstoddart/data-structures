015d919bbb877081dbfa1962971be0427851e06e
Isabel Stoddart <47155912+isabelstoddart@users.noreply.github.com>
2020-09-08 21:09:40 +0000

    Create wa01loop.js

---
+++ wa01/wa01loop.js
@@ -0,0 +1,22 @@
+for (var i = 1; i <= 9; i ++)
+{
+    if(i < 10) {
+        var filenum = "m0" + i
+    }
+    else {
+        var filenum = "m" + i
+    }
+    var request = require('request');
+    var fs = require('fs');
+    
+    var inFile = "https://parsons.nyc/aa/" + filenum + ".html"
+    var outFile = "/home/ec2-user/environment/AAMeetings/" + filenum + ".text"
+    console.log(outFile)
+    
+    request(inFile, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+        fs.writeFileSync(outFile, body);
+    }
+    else {console.log("Request failed!")}
+    });
+}
