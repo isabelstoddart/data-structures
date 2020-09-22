6205de73b5f0700d4e9d50c1cd70d54c6084947b
Isabel Stoddart <47155912+isabelstoddart@users.noreply.github.com>
2020-09-22 20:51:49 +0000

    Create wa03.js

---
+++ wa03/wa03.js
@@ -0,0 +1,63 @@
+"use strict";
+
+// dependencies
+const fs = require('fs'),
+      querystring = require('querystring'),
+      request = require('request'),
+      async = require('async'),
+      dotenv = require('dotenv');
+
+// TAMU api key
+dotenv.config();
+const API_KEY = process.env.TAMU_KEY;
+const API_URL = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx'
+
+// geocode addresses
+let meetingsData = [];
+let addresses = fs.readFileSync('/home/ec2-user/environment/AAMeetings/AAMeetingsAddresses.JSON');
+//puts JSON file back in its original array format
+addresses = JSON.parse(addresses)
+
+// eachSeries in the async module iterates over an array and operates on each item in the array in series
+async.eachSeries(addresses, function(value, callback) {
+    let query = {
+        streetAddress: value,
+        city: "New York",
+        state: "NY",
+        apikey: API_KEY,
+        format: "json",
+        version: "4.01"
+    };
+
+    //construct a querystring from the `query` object's values and append it to the api URL
+    let apiRequest = API_URL + '?' + querystring.stringify(query);
+    console.log(apiRequest);
+
+    request(apiRequest, function(err, resp, body) {
+        if (err){ throw err; }
+
+        let tamuGeo = JSON.parse(body);
+        console.log(tamuGeo['FeatureMatchingResultType'], apiRequest);
+        
+        let address = {
+            "address": '',
+            "latlong": {
+                "lat" : '',
+                "lng" : ''
+            }
+        }
+        
+        address.address = tamuGeo['InputAddress']['StreetAddress'];
+        address.latlong.lat = tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Latitude'];
+        address.latlong.lng = tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Longitude'];
+    
+        meetingsData.push(address);
+    });
+
+    //sleep for a couple seconds before making the next request
+    setTimeout(callback, 2000);
+}, function() {
+    fs.writeFileSync('AAMeetings/AAaddressesObj.json', JSON.stringify(meetingsData));
+    console.log('*** *** *** *** ***');
+    console.log(`Number of meetings in this zone: ${meetingsData.length}`);
+});
