1f583728288577f3d8ea424484cd1d302248a33d
Isabel Stoddart <47155912+isabelstoddart@users.noreply.github.com>
2020-09-22 20:08:08 +0000

    Create wa02_2.js

---
+++ wa02/wa02_2.js
@@ -0,0 +1,24 @@
+//redone wa02 printing out a JSON file instead of a txt file
+
+// npm install cheerio
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+// load the thesis text file into a variable, `content`
+var content = fs.readFileSync('AAMeetings/m4.txt');
+
+// load `content` into a cheerio object
+var $ = cheerio.load(content);
+
+// write the addresses to a text file
+var AAMeetingsAddresses = [];
+
+$('td').each(function(i, elem) {
+    if($(elem).attr("style")=="border-bottom:1px solid #e3e3e3; width:260px") {
+        AAMeetingsAddresses.push((($(elem).html().split('<br>')[2].trim().split(',')[0])));
+    }
+});
+
+for(let i = 0; i < AAMeetingsAddresses.length; i++){
+    fs.writeFileSync('AAMeetings/AAMeetingsAddresses.JSON', JSON.stringify(AAMeetingsAddresses))
+}
