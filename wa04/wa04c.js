eed1186db62be14b8207028f9361adc2fe399d74
EC2 Default User <ec2-user@ip-172-31-50-251.ec2.internal>
2020-09-29 04:27:42 +0000

    add wa04

---
+++ wa04/wa04c.js
@@ -0,0 +1,24 @@
+const { Client } = require('pg');  
+const dotenv = require('dotenv');
+dotenv.config({path: '/home/ec2-user/environment/data-structures/.env'});  
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'isabelmsdv';
+db_credentials.host = 'data-structures.cih8sqm2fyre.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'aa';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+// Connect to the AWS RDS Postgres database
+const client = new Client(db_credentials);
+client.connect();
+
+// Sample SQL statement to query the entire contents of a table: 
+//var thisQuery = "SELECT count(*) FROM aalocations;";
+var thisQuery = "Select * FROM aalocations;";
+
+client.query(thisQuery, (err, res) => {
+    console.log(err, res.rows);
+    client.end();
+});
